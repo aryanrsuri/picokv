86c96c3328c94bf995666bf6622c906dfecbaf82
Aryan Suri <arysuri@proton.me>
2024-06-01 15:48:28 -0700

    added putting data to an existsting key\n is this good?
    Signed-off-by: Aryan Suri <arysuri@proton.me>

--- kv.py
+++ kv.py
@@ -1,21 +1,25 @@
 import hashlib
 import os
 
-def put(key, value):
+def put(key: str, value: str):
     h = hash(key)
-    if os.path.isfile(os.path.join("/tmp", hash(key))): return None 
-    with open(os.path.join("/tmp", h), "wb+") as bin:
+    # if os.path.isfile(os.path.join("/tmp", hash(key))): return None 
+    with open(os.path.join("/tmp", h), "ab+") as bin:
         bin.write(value.encode("utf-8"))
     return h
 
-def get(key):
-    with open(os.path.join("/tmp", hash(key)), "rb") as bin:
-        return bin.read()
+def get(key: str):
+    try:
+        with open(os.path.join("/tmp", hash(key)), "rb") as bin:
+            return bin.read()
+    except Exception:
+        return b'Error\n'
 
 def hash(key: str):
     h = hashlib.new('sha256')
     h.update(str.encode(key))
     return h.hexdigest()
+
 
 
 
